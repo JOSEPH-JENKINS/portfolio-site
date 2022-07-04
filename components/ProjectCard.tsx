43f0883582e2a36fab4615013fd75e31b1821e54
Joseph Jenkins <amjosephjenkins@gmail.com>
2022-07-04 14:24:57 -0700

    commit: styling cards

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,7 +7,7 @@ import imageLoader from "../imageLoader"
 
 const ProjectCard = ({ first, last }: ProjectData) => {
   return (
-    <div className="mb-48">
+    <>
         <div className="head mb-12 col-start-6 col-end-13" data-scroll>
             <h1 className='text-right'>
                 <span className='font-bold text-9xl'>{first}</span>
@@ -17,12 +17,12 @@ const ProjectCard = ({ first, last }: ProjectData) => {
         <div className="col-start-4 col-end-13 bg-black h-[80vh] relative -z-10" data-scroll>
             <Image loader={imageLoader} unoptimized src="/Capture.png" alt="Yeah" layout='fill' />
         </div>
-        <div className='col-start-1 col-end-5' data-scroll>
+        <div className='col-start-1 col-end-5 mb-48' data-scroll>
             <p>Worked on redesigning the Axer Studios site. This project was done out of fun after applying for the frontend developer internship role at Axer Studios. 
                 <Link href={"/"}>Click to know more &rarr;</Link>
             </p>
         </div>
-    </div>
+    </>
   )
 }
 
