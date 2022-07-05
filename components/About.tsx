a74556a92ca1ccfe9d07ed1c77ba1fae1dd91c5b
Joseph Jenkins <amjosephjenkins@gmail.com>
2022-07-05 14:06:19 -0700

    commit: text alignment

---
+++ components/About.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+
+const About: FC = () => {
+  return (
+    <section className="mt-24 w-full relative mb-24" data-scroll-section>
+        <div className="block sm:grid grid-cols-6 gap-5">
+            <h1 className="font-sans text-base">About Me</h1><br />
+            <div className='font-bold w-full ml-auto text-lg col-start-3 col-end-7'>
+                <h1>
+                    I am a front-end developer based in Lagos, Nigeria who is driven to creating empathic experiences on the world wide web.
+                    Passionate about web technologies. I love working with a careful thought-out design and bringing that design to life with those web technologies.
+                    <br /><br />
+                    When I'm not creating intriguing web experiences, i'm probably watching anime or reading something.
+                </h1>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useRef } from 'react'
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import About from '../components/About'
 import Cursor from '../components/Cursor'
 import Intro from '../components/Intro'
 import Nav from '../components/Nav'
@@ -22,19 +23,7 @@ const Home: NextPage = () => {
         <Nav />
         <main className='w-full px-5 sm:px-12'>
           <Intro />
-          <section className="mt-48 w-full relative" data-scroll-section>
-            <div className="block sm:grid grid-cols-6 gap-5">
-              <h1 className="font-sans text-sm">About Me</h1><br />
-              <div className='font-bold w-full ml-auto text-lg col-start-3 col-end-7'>
-                <h1>
-                  I am a front-end developer based in Lagos, Nigeria who is driven to creating empathic experiences on the world wide web.
-                  Passionate about web technologies. I love working with a careful thought-out design and bringing that design to life with those web technologies.
-                  <br /><br />
-                  When I'm not creating intriguing web experiences, i'm probably watching anime or reading something.
-                </h1>
-              </div>
-            </div>
-          </section>
+          <About />
         </main>
       </div>
     </>
